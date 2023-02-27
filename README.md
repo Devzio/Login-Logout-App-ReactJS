fe7756698bc4dc9fa96b71458580e2a90e083b5c
Devzio <Jasdevbedi@gmail.com>
2023-02-27 17:55:18 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Google-Login-Authentication
+ A simple login logout project handled by firebase google authentication made with React Js and Typescript
---
+++ src/config/config.ts
@@ -0,0 +1,12 @@
+const config = {
+    firebase: {
+        apiKey: "AIzaSyATbp8jZR_m6oK16d4Vd2AuGHGsLr1jE10",
+        authDomain: "login-auth-f3389.firebaseapp.com",
+        projectId: "login-auth-f3389",
+        storageBucket: "login-auth-f3389.appspot.com",
+        messagingSenderId: "206266738161",
+        appId: "1:206266738161:web:80a5a0dc99482689cd1050"
+    }
+};
+
+export default config;
